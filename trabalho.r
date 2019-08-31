d2d317b16de749ea797ec8be4ccbff359e48617d
Felipe Guimarães <felipeguima4@gmail.com>
2019-08-31 14:38:51 -0300

    Mudanças simples

--- trabalho.r
+++ trabalho.r
@@ -8,7 +8,37 @@ head(dados)
 
 sexo
 
+dados$sexo
+
 attach(dados)
 
 sexo
+
+t1<- table(sexo)
+t1
+
+t2<- round(prop.table(t1)*100, 2)
+t2
+
+leg= c("Masculino", "Feminino");
+cores= rainbow(length(leg));
+pie(t1, labels= t2, col= cores);
+legend("topright", leg, cex= 0.8, fill= cores);
+
+t1<-table(curso)
+t1
+t2<- round(prop.table(t1)*100, 2)
+t2
+#	NÃO É O MELHOR CASO USAR GRAFICOS DE PIZZA
+#leg= c("Eng. Espacial", "Eng. Ambiental", "Eng. Controle", "Eng. Minas", "Eng. Producao", 
+#	 "Eng. Sistemas", "Eng. Mecanica", "Eng. Metalurgica", "Geologia", "Lic. Matematica", 
+#	 "Matematica", "Quimica", "SI") 
+#leg
+#cores= rainbow(length(leg))
+#pie(t1, labels=t2, col=cores)
+#legend("bottomleft", leg, cex=0.8, fill=cores)
+
+
+
+
 
