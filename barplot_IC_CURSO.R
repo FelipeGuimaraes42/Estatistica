637bb8b9b717d0c51f285f1214ff9c6a62df1fdd
Lucas Marinho <42285408+lucasmarinhomr@users.noreply.github.com>
2019-09-04 11:03:57 -0300

    Add files via upload

---
+++ barplot_IC_CURSO.R
@@ -0,0 +1,33 @@
+dados <- read.csv(
+	file = "C:/Users/lucasmms/Documents/GitHub/Estatistica/code.csv",
+	sep = ";",
+	dec = ",",
+	header=T
+	)
+
+attach(dados)
+
+t1 <- table(ic,curso)
+t1 <- round(prop.table(t1,margin=2)*100,2)
+
+legendaCurso = c("Eng. Espacial", "Eng. Ambiental", "Eng. Controle", "Eng. Minas", "Eng. Produção",
+	 "Eng. Sistemas", "Eng. Mecânica", "Eng. Metalúrgica", "Geologia", "Lic. Matemática", 
+	 "Matemática", "Química", "Sist. Informação")
+
+cor = rainbow(length(legendaCurso))
+
+##
+##NOMES SÃO PLOTADOS MUITO PEQUENOS
+##
+
+barCursoIc <- barplot(
+	t1,
+	names.arg = legendaCurso,
+ 	ylab = "Iniciacao Cientifica (%)", 
+	xlab = "Cursos", 
+	ylim = c(0,101), 
+	xlim = c(0,15), 
+	cex.names=0.25, 
+	col = cor, 
+	xaxs="i"
+	)
